b683bd7deba5cc610bcfdb6ae2183f3e748f38a6
muno06 <121367035+muno06@users.noreply.github.com>
2023-03-22 16:19:49 +0900

    3.22일차 수업

---
+++ GuGuDan.py
@@ -0,0 +1,10 @@
+
+i = 0
+while i < 9:
+    j = 0
+    while j < 9:
+        print( i + 1, '*', j + 1, '=', (i + 1) * j + 1, end=', ')
+        j += 1
+    print()
+    i += 1
+
---
+++ Turtle01.py
@@ -0,0 +1,16 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.shape("turtle")
+
+t.left(36)
+t.forward(300)
+t.left(144)
+t.forward(300)
+t.left(144)
+t.forward(300)
+t.left(144)
+t.forward(300)
+t.left(144)
+t.forward(300)
